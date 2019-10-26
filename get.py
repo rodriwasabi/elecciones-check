c01b9f0254abd46189f40170e3182ca40a06cb7b
Rodrigo Caballero <rodrigo.caballero@aurea.com>
2019-10-26 10:26:27 -0400

    adding change to get.py

--- get.py
+++ get.py
@@ -3,25 +3,27 @@ import requests
 
 baseUrl = "https://trep.oep.org.bo/resul/imgActa/{0}.jpg"
 
-def downloadImages(group, tables):
+def downloadImages(group, tables, addSuffixOne):
     tablesWithError = []
     for table in tables:
-        tableNumber = table.replace('\n', '')
-        if re.search('^[0-9]+$', tableNumber): 
+        tableNumber = table.replace('\n', '')        
+        if re.search('^[0-9]+$', tableNumber):
+            if addSuffixOne == True:
+                tableNumber = "{}1".format(tableNumber)
             getUrl = baseUrl.format(tableNumber)
             imageFile = './results/{0}/{1}.jpg'.format(group, tableNumber)
-            print getUrl
-            r = requests.get(getUrl, stream=True)
-            if r.status_code == 200:
-                with open(imageFile, 'wb') as f:
-                    for chunk in r:
-                        if 'ERROR' in chunk:
-                            print "ReDo needed: {}".format(tableNumber)
-                            tablesWithError.append(tableNumber)
-                        f.write(chunk)
-                # Deletes file if it is a file with error
-                if tableNumber in tablesWithError:
-                    os.remove(imageFile)
+            print getUrl            
+            # r = requests.get(getUrl, stream=True)
+            # if r.status_code == 200:
+            #     with open(imageFile, 'wb') as f:
+            #         for chunk in r:
+            #             if 'ERROR' in chunk:
+            #                 print "ReDo needed: {}".format(tableNumber)
+            #                 tablesWithError.append(tableNumber)
+            #             f.write(chunk)
+            #     # Deletes file if it is a file with error
+            #     if tableNumber in tablesWithError:
+            #         os.remove(imageFile)
 
     return tablesWithError
 
@@ -53,18 +55,18 @@ def scrap(args):
             tables = fp.readlines()
 
         # r=root, d=directories, f = files
-        tablesWithError = downloadImages(group, tables)
+        tablesWithError = downloadImages(group, tables, True)
 
         if len(tablesWithError) > 0:
             
             for tableWithError in tablesWithError:
                 with open(errorFile, 'a') as f:
-                    f.write("{}1\n".format(tableWithError))
+                    f.write("{}\n".format(tableWithError))
 
             if len(tablesWithError) > 0:
                 with open(errorFile) as fp:
                     tablesWithErrorFromFile = fp.readlines()
-                tablesWithError = downloadImages(group, tablesWithErrorFromFile)
+                tablesWithError = downloadImages(group, tablesWithErrorFromFile, False)
                 if len (tablesWithError) > 0:
                     print "still tables with error: "
                     for tableError in tablesWithError:
